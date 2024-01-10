3079ee0416e410d833f6729ef5d730ee4947d0b5
Chung Min Kim <chungmin99@gmail.com>
2024-01-09 21:01:48 -0800

    Tentative main results freeze

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var video_width = 960;
 const VIDEO_ASPECT_RATIO = 16.0 / 9.0;
 var display_level = 2;
-var video_names = ["keyboard", "louvre", "bouquet", "donuts", "dozer", "succulent"];
+var video_names = ["keyboard", "succulent", "louvre", "bouquet", "donuts", "dozer", "cafe"];
     
 var videos = [];
 var current_video_idx = 0;
